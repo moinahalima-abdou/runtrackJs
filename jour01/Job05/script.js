7fd369ec992a937257dcedda10259a94e43ec1c3
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-04-21 06:42:48 +0200

    Job 05 - display days of the week using a loop

---
+++ jour01/Job05/script.js
@@ -0,0 +1,19 @@
+function afficherJoursSemaines(){
+
+    let joursSemaines = [
+        "lundi",
+        "mardi",
+        "mercredi",
+        "jeudi",
+        "vendredi",
+        "samedi",
+        "demanche"
+    ];
+
+    for (let i = 0; i < joursSemaines.length; i++){
+        console.log(joursSemaines[i]);
+    }
+}
+
+// appel de la fonction
+afficherJoursSemaines();
