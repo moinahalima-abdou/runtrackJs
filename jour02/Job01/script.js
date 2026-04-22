d83f0364e1ec6e6c7ab970d1e5266ec910e315f2
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-04-22 07:00:45 +0200

    Update job name from Job 2 to Job 1 in job01 folder

---
+++ jour02/Job01/script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+
+    let citation = document.getElementById("citation").textContent;
+
+    console.log(citation);
+
+});
